20d71990d721b9f53462ba4de101f75b0a32111b
prasannakumarSPK <32704685+prasannakumarSPK@users.noreply.github.com>
2018-06-29 07:39:55 -0500

    Add files via upload

---
+++ week1/day 04/Findelementinorderset.java
@@ -0,0 +1,112 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution {
+
+    public static boolean contains(int[] nums, int target) {
+        
+        
+    
+
+        int floorIndex = -1;
+        int ceilingIndex = nums.length;
+
+        while (floorIndex + 1 < ceilingIndex) {
+
+            int halfDistance = (ceilingIndex - floorIndex)/ 2;
+            int guessIndex = floorIndex + halfDistance;
+
+            int guessValue = nums[guessIndex];
+
+            if (guessValue == target) {
+                return true;
+            }
+
+            if (guessValue > target) {
+
+                ceilingIndex = guessIndex;
+
+            } else {
+
+                floorIndex = guessIndex;
+            }
+        }
+
+        return false;
+    }
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void emptyArrayTest() {
+        final boolean result = contains(new int[] {}, 1);
+        assertFalse(result);
+    }
+
+    @Test
+    public void oneItemArrayNumberPresentTest() {
+        final boolean result = contains(new int[] {1}, 1);
+        assertTrue(result);
+    }
+
+    @Test
+    public void oneItemArrayNumberAbsentTest() {
+        final boolean result = contains(new int[] {1}, 2);
+        assertFalse(result);
+    }
+
+    @Test
+    public void smallArrayNumberPresentTest() {
+        final boolean result = contains(new int[] {2, 4, 6}, 4);
+        assertTrue(result);
+    }
+
+    @Test
+    public void smallArrayNumberAbsentTest() {
+        final boolean result = contains(new int[] {2, 4, 6}, 5);
+        assertFalse(result);
+    }
+
+    @Test
+    public void largeArrayNumberPresentTest() {
+        final boolean result = contains(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 8);
+        assertTrue(result);
+    }
+
+    @Test
+    public void largeArrayNumberAbsentTest() {
+        final boolean result = contains(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 0);
+        assertFalse(result);
+    }
+
+    @Test
+    public void largeArrayNumberFirstTest() {
+        final boolean result = contains(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1);
+        assertTrue(result);
+    }
+
+    @Test
+    public void largeArrayNumberLastTest() {
+        final boolean result = contains(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10);
+        assertTrue(result);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
